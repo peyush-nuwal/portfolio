8af95a57c2864114658b3e4c982386a4c010c94b
peyush <piyushnawal19.com>
2025-04-28 17:21:58 +0530

    add nav video link

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -237,7 +237,7 @@ const Nav = forwardRef<HTMLDivElement>(
             <div className="w-full lg:w-1/2  h-1/2 lg:h-full   flex items-center justify-start  pl-[5%] ">
               <div ref={videoRef} className=" w-[50%] h-[50%]  overflow-hidden">
                 <video
-                  src="/ODETARI.mp4"
+                  src="https://github.com/peyush-nuwal/portfolio/blob/master/public/ODETARI.mp4"
                   autoPlay
                   muted
                   loop
