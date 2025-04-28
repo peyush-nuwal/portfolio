b343b9e7fe72e075edfe208b29df320bda3fd1e9
peyush <piyushnawal19.com>
2025-04-28 17:56:22 +0530

    used webpack analyzer to check app load

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* your config options here */
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
